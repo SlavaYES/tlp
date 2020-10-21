fca8c5537d3f9b40ca64d43cc9bc6ba571147c4c
SlavaYES <voen1999@gmail.com>
2020-10-22 01:06:42 +0700

    just do it, I not know how translate with Form on Component Blazor

---
+++ lab1/lab_1_Interface/Models/DeterministicAutomationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_1_Interface.Models
+{
+    public class DeterministicAutomationModel
+    {
+        public enum AllStates { Recognized = 0, Good = 1, BadSymbol = 2, EmptyCellRecognized = 3, EmptyCellGood = 4 };
+        public string[] statusString { get; set; }
+        public Dictionary<string, Dictionary<string, string>> lang { get; set; }
+        public List<string> states { get; set; }
+        public List<string> terminal { get; set; }
+        public List<string> endStates { get; set; }
+    }
+}
---
+++ lab1/lab_1_Interface/Pages/Determ_auto/DetermAuto.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lab_1_Interface.Models;
+
+namespace lab_1_Interface.Pages.Determ_auto
+{
+    public partial class DetermAuto
+    {
+        DeterministicAutomationModel DeterAuto= new DeterministicAutomationModel();
+        string ErrorMessage;
+        protected override void OnInitialized()
+        {
+            DeterAuto.lang = new Dictionary<string, Dictionary<string, string>>();
+            DeterAuto.states = new List<string>();
+            DeterAuto.terminal = new List<string>();
+            DeterAuto.endStates = new List<string>();
+            DeterAuto.statusString = new string[5] {
+                "Цепочка принадлежит",
+                "Цепочка не пренадлежит",
+                "Неверный Символ",
+                "Нет перехода, цепочка принадлежит",
+                "Нет перехода, цепочка не принадлежит"
+            };
+        }
+
+        public void ShowError(string error)
+        {
+            ErrorMessage = error;
+        }
+
+        public void addRowClick(object s, EventArgs e)
+        {
+            //if () {
+            //    ShowError("Сначала добавьте как минимум один столбец!");
+            //    return;
+            //}
+        }
+    }
+}
